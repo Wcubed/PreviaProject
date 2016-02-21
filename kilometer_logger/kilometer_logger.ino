4bdee3d0672d81809e145af3551d739d168bc15a
hesje <h.bierma@me.com>
2016-02-21 18:03:34 +0100

    Now reads uuid

--- kilometer_logger/kilometer_logger.ino
+++ kilometer_logger/kilometer_logger.ino
@@ -3,10 +3,10 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-const int RST_PIN = 9;           // Configurable, see typical pin layout above
-const int SS_PIN = 8;          // Configurable, see typical pin layout above
+const int rfid_RST_PIN = 9;           // Configurable, see typical pin layout above
+const int rfid_SS_PIN = 8;          // Configurable, see typical pin layout above
 
-MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522 rfid(rfid_SS_PIN, rfid_RST_PIN);
 
 void setup() {
 
@@ -23,10 +23,18 @@ void loop() {
     if (rfid.PICC_ReadCardSerial()){
 
       Serial.print("Card UID: ");
-      
+      print_byte_array(rfid.uid.uidByte, rfid.uid.size);
+      Serial.println();
       
     }
     
   }
 
+}
+
+void print_byte_array(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
 }
