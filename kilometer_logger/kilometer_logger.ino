cb2318cdff5f665e675b9e7444f2841bc3a98326
hesje <h.bierma@me.com>
2016-02-21 17:55:47 +0100

    SD, Softserial and SPI libs, init code added

--- kilometer_logger/kilometer_logger.ino
+++ kilometer_logger/kilometer_logger.ino
@@ -1,8 +1,32 @@
+#include <SoftwareSerial.h>
+#include <SD.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+const int RST_PIN = 9;           // Configurable, see typical pin layout above
+const int SS_PIN = 8;          // Configurable, see typical pin layout above
+
+MFRC522 rfid(SS_PIN, RST_PIN);
 
 void setup() {
-  
+
+  Serial.begin(115200);
+  SPI.begin();
+  rfid.PCD_Init();
+
 }
 
 void loop() {
-  
+
+  if (rfid.PICC_IsNewCardPresent()) {
+
+    if (rfid.PICC_ReadCardSerial()){
+
+      Serial.print("Card UID: ");
+      
+      
+    }
+    
+  }
+
 }
