ca8e7d4e8a36cb703eca447f0616e905351928cd
hesje <h.bierma@me.com>
2016-02-21 18:32:30 +0100

    Only reads the same card every 5 seconds.

--- kilometer_logger/kilometer_logger.ino
+++ kilometer_logger/kilometer_logger.ino
@@ -8,6 +8,10 @@ const int rfid_SS_PIN = 8;          // Configurable, see typical pin layout abov
 
 MFRC522 rfid(rfid_SS_PIN, rfid_RST_PIN);
 
+unsigned long lastMillis;
+int lastCardCountdown = 0;
+byte lastCardUID[4];
+
 void setup() {
 
   Serial.begin(115200);
@@ -17,24 +21,54 @@ void setup() {
 }
 
 void loop() {
+  // Calculate delta time.
+  unsigned long curMillis = millis();
+  unsigned int dt = curMillis - lastMillis;
+  lastMillis = curMillis;
+
+  if (lastCardCountdown > 0) {
+    lastCardCountdown -= dt;
+  }
 
   if (rfid.PICC_IsNewCardPresent()) {
 
-    if (rfid.PICC_ReadCardSerial()){
+    if (rfid.PICC_ReadCardSerial()) {
 
-      Serial.print("Card UID: ");
-      print_byte_array(rfid.uid.uidByte, rfid.uid.size);
-      Serial.println();
-      
+      if (!are_uids_equal(lastCardUID, rfid.uid.uidByte) || lastCardCountdown <= 0) {
+        lastCardCountdown = 5000;
+        
+        for (int i = 0; i < 3; i++) {
+          lastCardUID[i] = rfid.uid.uidByte[i]; //copy UID into lastcarduid
+        }
+
+        Serial.print("Card UID: ");
+        print_byte_array(rfid.uid.uidByte, rfid.uid.size); //print UID of card
+        Serial.println();
+      }
+
     }
-    
+
   }
+
+
 
 }
 
-void print_byte_array(byte *buffer, byte bufferSize) {
-    for (byte i = 0; i < bufferSize; i++) {
-        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-        Serial.print(buffer[i], HEX);
+// Checks if two UIDs are equal.
+bool are_uids_equal(byte uid1[4], byte uid2[4]) {
+  for (int i = 0; i < 3; i++) {
+    if (uid1[i] != uid2[i]) {
+      return false;
     }
+  }
+
+  return true;
+}
+
+// Prints byte arrays to the serial monitor.
+void print_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
 }
